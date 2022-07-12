58af9b1aa54cd479ff55fee366d2ce0a8142ce09
DariaDovydova <daria.dovydova@gmail.com>
2022-07-12 17:31:10 +0300

    gh-pages added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,9 +4,9 @@ function Header() {
   return (
     <nav className="grey darken-4">
             <div className="nav-wrapper">
-                <a href="!#" className="brand-logo">React Shop</a>
+                <a href="/" className="brand-logo">React Shop</a>
                 <ul id="nav-mobile" className="right hide-on-med-and-down">
-                    <li><a href="!#">REPO</a></li>
+                    <li><a href="https://github.com/DariaDovydova/React-Shop" target='_blank' rel="noreferrer">REPO</a></li>
                 </ul>
             </div>
         </nav>
